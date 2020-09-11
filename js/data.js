ad5b63f2ad8f2e2a0e3e07251467aa1888f0493e
tranglu <lu.t.t.trang@gmail.com>
2020-09-11 20:46:31 +0700

    11/09/2020

---
+++ js/data.js
@@ -0,0 +1,39 @@
+function getTeamMembers() {
+    let data = [{
+            imagepath: 'images/t1.jpg',
+            summary: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, perferendis!'
+        },
+        {
+            imagepath: 'images/t2.jpg',
+            summary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, placeat?'
+        },
+        {
+            imagepath: 'images/t3.jpg',
+            summary: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, harum.'
+        }
+    ];
+    return data;
+}
+
+function getGalleryImages() {
+    const data = [{
+            imagepath: 'images/g1.jpg'
+        },
+        {
+            imagepath: 'images/g2.jpg'
+        },
+        {
+            imagepath: 'images/g3.jpg'
+        },
+        {
+            imagepath: 'images/g4.jpg'
+        },
+        {
+            imagepath: 'images/g5.jpg'
+        },
+        {
+            imagepath: 'images/g6.jpg'
+        }
+    ];
+    return data;
+}
